47eed19f7e2f64b9b4e787ca8f31244b84896cf0
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-05-21 19:50:02 +0500

    add routes settings

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,5 +1,35 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import sidebar from "../utils/sidebar";
+
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <Routes>
+      {sidebar.map((parent) => {
+        const ParentElement = parent.element;
+        if (parent?.children) {
+          return parent.children.map((child) => {
+            const ChildElement = child.element;
+            return (
+              <Route
+                key={child.id}
+                path={child.path}
+                element={<ChildElement />}
+              />
+            );
+          });
+        } else
+          return (
+            <Route
+              key={parent.id}
+              path={parent.path}
+              element={<ParentElement />}
+            />
+          );
+      })}
+      <Route path="/" element={<Navigate to={"/analitika"} />} />
+      <Route path="*" element={<h1>NOT FOUND 404</h1>} />
+    </Routes>
+  );
 };
 
 export default Root;
--- src/view/Generic/index.jsx
+++ src/view/Generic/index.jsx
@@ -1,14 +1,12 @@
 import { useLocation } from "react-router-dom";
 import { Container } from "./style";
 
-const Generics = () => {
+export const Generics = () => {
   const location = useLocation();
   return (
     <Container>
-      <h1>{location.pathname}</h1>
+      <h1>{location.pathname} page</h1>
       <h1>Cooming soon....</h1>
     </Container>
   );
 };
-
-export default Generics;
