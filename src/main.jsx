ac75e81986fffd706bd16a00ba5ff45c0520ef35
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-05-21 19:23:38 +0500

    add ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Root from "./root";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hellow world</h1>
+    <Root />
   </React.StrictMode>
 );
